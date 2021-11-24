0ec7c3a85ef87d7a6d79c7743c75c78a55f5b409
dekeyofspace <83281012+dekeyofspace@users.noreply.github.com>
2021-11-24 10:31:50 +0300

    Add files via upload

---
+++ Meditation/Meditation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meditation
+//
+//  Created by Student on 24.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
